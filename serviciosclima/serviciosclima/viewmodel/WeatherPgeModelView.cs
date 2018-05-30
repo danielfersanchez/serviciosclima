8b137d8e802412d4a8d9aa39cfd209119a5b4276
danielfersanchez <38057662+danielfersanchez@users.noreply.github.com>
2018-05-29 20:26:09 -0500

    commit

--- serviciosclima/serviciosclima/viewmodel/WeatherPgeModelView.cs
+++ serviciosclima/serviciosclima/viewmodel/WeatherPgeModelView.cs
@@ -1,10 +1,143 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace serviciosclima.viewmodel
 {
-    class WeatherPgeModelView
+    class WeatherPgeModelView : notificable
     {
+        #region atributos
+        private string ubicacion;
+        private string pais;
+        private string resultTerm;
+        private string region;
+        private string ultimaactualizacion;
+        private string clima;
+        private string temperatura;
+        private ImageSource image;
+
+        #endregion
+        #region propiedades
+        public string Region
+        {
+            get
+            {
+                return region;
+            }
+            set
+            {
+                SetValue(ref region, value);
+            }
+        }
+
+        public string UltimaActualizacion
+        {
+            get
+            {
+                return ultimaactualizacion;
+            }
+            set
+            {
+                SetValue(ref ultimaactualizacion, value);
+            }
+        }
+        public string Clima
+        {
+            get
+            {
+                return clima;
+            }
+            set
+            {
+                SetValue(ref clima, value);
+            }
+        }
+
+        public string Temperatura
+        {
+            get
+            {
+                return temperatura;
+            }
+            set
+            {
+                SetValue(ref temperatura, value);
+            }
+        }
+
+        public string Ubicacion
+        {
+            get
+            {
+                return ubicacion;
+            }
+            set
+            {
+                SetValue(ref ubicacion, value);
+            }
+        }
+        public string Pais
+        {
+            get
+            {
+                return pais;
+            }
+            set
+            {
+                SetValue(ref pais, value);
+            }
+        }
+        public string ResultTerm
+        {
+            get
+            {
+                return resultTerm;
+            }
+            set
+            {
+                SetValue(ref resultTerm, value);
+            }
+        }
+        public ImageSource Imagen
+        {
+            get
+            {
+                return image;
+            }
+            set
+            {
+                SetValue(ref image, value);
+            }
+        }
+        #endregion
+
+        #region comadoso
+        public  ICommand BuscarCommand
+        {
+            get
+            {
+                return new RelayCommand(Buscar);
+            }
+        }
+
+        #endregion
+        #region
+        public WeatherPgeModelView()
+        {
+
+        }
+        #endregion
+
+        #region METODOS
+
+        private void Buscar()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
--- serviciosclima/serviciosclima/viewmodel/notificable.cs
+++ serviciosclima/serviciosclima/viewmodel/notificable.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace serviciosclima.viewmodel
 {
-    class notificable
+    public class notificable: INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnProperty([CallerMemberName] String propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected void SetValue<T>(ref T backingField, T value, [CallerMemberName] string propertyName= null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingField, value))
+            {
+                return;
+            }
+            backingField = value;
+            onPropertyChanged(propertyName);
+        }
+
+        private void onPropertyChanged(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
